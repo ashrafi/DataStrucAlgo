f3d289f22c1dc1a451913b4b7dccffc40aa94712
YLabz Developer <developer@ylabz.com>
2022-04-14 21:11:17 -0700

    Start working with Big O

---
+++ README.md
@@ -0,0 +1,6 @@
+Docs
+https://docs.google.com/document/d/10fyoaJGAERtJZY2DFNDNBzs-pk00SV1DUpU_XJuioNA/edit#
+
+Kotlin
+https://developerlife.com/2018/08/16/algorithms-in-kotlin-1/
+
--- src/main/kotlin/BigO.kt
+++ src/main/kotlin/BigO.kt
@@ -1,2 +1,31 @@
 class BigO {
+
+    /**
+     * To meaningfully compare algorithmic performance, we can use big O notation – sometimes referred to as “order of growth.”
+     * In short, it compares the asymptotic behavior of algorithms; that is, how does their performance scale as a function of the input size?
+     */
+
+    /**
+     * O(1)
+     * An algorithm that will always execute in the same time (or space) regardless of the size of the input data set.
+     */
+
+    fun isFirstElementNull(list: List<String?>) = list[0]==null
+
+    /**
+     * O(n)
+     * An algorithm whose performance will grow linearly and in direct proportion to the size of the input data set.
+     * Big O favors the worst-case performance scenario.
+     */
+
+
+    // Big O favours the worst-case performance scenario
+    fun containsValue(list: List<String>, value: String): Boolean {
+        list.forEach { it ->
+            if (it == value) return true
+        }
+        return false
+    }
+
+
 }
